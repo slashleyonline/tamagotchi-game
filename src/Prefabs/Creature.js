13debd6db26bce246db51ac320af4cd163e45354
NonDrip <dseward@ucsc.edu>
2026-03-11 01:22:03 -0700

    added time that decrements stats.

--- src/Prefabs/Creature.js
+++ src/Prefabs/Creature.js
@@ -6,5 +6,31 @@ class Creature extends Phaser.Physics.Arcade.Sprite {
         this.parentScene = scene
         this.parentScene.add.existing(this)
         this.scale = 0.4
+
+
+        this.health = 100
+        //overall health of the creature
+        //the average of all stats
+        //if it reaches 0, game over
+
+        this.happiness = 10
+        //increases gradually but the rate of increasing is maintained by playing with creature
+
+        this.sleep = 100
+        //refilled by sleeping
+        
+        this.hunger = 20
+        //refilled by eating
+
+    }
+    incrementTime() {
+        this.happiness -= 0.1
+        this.sleep -= 0.1
+        this.hunger = 0.1
+
+        this.health = (happiness + sleep + hunger) / 3
+        if (this.health == 0) {
+            console.log('game over!')
+        }
     }
 }
--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -12,5 +12,23 @@ class Play extends Phaser.Scene {
         ash.play('idle', true)
 
         let quitButton = new MenuButton(this, 600, 300, 'quitButton')
+
+        this.time = new Date()
+        this.timeText = this.add.text(game.CENTER_X + 70, (game.config.height / 4), this.time.getHours(), {
+            align: "center",
+            color: "black"
+        })
+        this.resetDate(this.timeText)
+        setInterval(this.resetDate, 1000, this.timeText)
+        setInterval(ash.incrementTime, 1000)
+    }
+
+    resetDate(timeText) {
+        this.time = new Date()
+
+        let hours = this.time.getHours()
+        let minutes = this.time.getMinutes()
+
+        timeText.text = new String(hours + ':' + minutes)
     }
 }
